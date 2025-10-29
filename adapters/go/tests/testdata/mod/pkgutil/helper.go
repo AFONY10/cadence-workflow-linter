c9c92f748736234e91c004e149134b96a82a7db1
AFONY10 <133364236+AFONY10@users.noreply.github.com>
2025-10-29 11:08:26 +0100

    added package

--- adapters/go/tests/testdata/mod/pkgutil/helper.go
+++ adapters/go/tests/testdata/mod/pkgutil/helper.go
@@ -4,10 +4,10 @@ import "time"
 
 // Helper returns the current time (intentionally uses time.Now to be flagged by detectors in tests)
 func Helper() time.Time {
-    return time.Now()
+	return time.Now()
 }
 
 // SafeHelper is a safe helper that doesn't use problematic APIs
 func SafeHelper() string {
-    return "safe"
+	return "safe"
 }
